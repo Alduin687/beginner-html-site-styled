9dbaab006d23be3e8c561575ee0cc7bbf6533596
Emmett Orduno <Emmett.Orduno24@student.cart.org>
2025-01-23 13:21:55 -0800

    Fit

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,2 +1,2 @@
 const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+myHeading.textContent = "The Fitness Gram Pacer test is a multi stage aerobic capacity test that progressively gets more difficult as it continues.";
